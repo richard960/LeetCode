2ca18df498d432f1329fe7e07a872964e343019c
richard960 <medinarichard996@gmail.com>
2021-12-19 22:28:09 -0800

    Time: 76 ms (51.01%), Space: 39.3 MB (11.00%) - LeetHub

--- length-of-last-word/length-of-last-word.js
+++ length-of-last-word/length-of-last-word.js
@@ -4,10 +4,8 @@
  */
 var lengthOfLastWord = function(s) {
     let arr = s.split(' ');
-    console.log(arr[arr.length -1])
     while(arr[arr.length -1] === '') {
         arr.pop();
     }
-    console.log(arr);
     return arr[arr.length - 1].length
 };
